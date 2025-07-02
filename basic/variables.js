2f585f640c0e9a5c47a975d92641541f3de95339
satishmankodiya01 <satishmankodiya01@gmail.com>
2025-07-02 11:17:56 +0000

    var test

---
+++ basic/variables.js
@@ -0,0 +1,12 @@
+const accountID = 12345
+let accountEmail = "satishm@test.com"
+let accountPassword = "12345678"
+let accountCity = "Jaipur"
+
+
+
+accountEmail = "abcsatishm@test.com"
+accountPassword = "21212121 "
+accountCity = "Mumbai"
+
+console.table([accountID, accountEmail, accountPassword, accountCity])
